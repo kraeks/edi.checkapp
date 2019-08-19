620a37000bf524c34db66a5e77775c1dc71312b7
lwalther <lwalther@novareto.de>
2019-08-19 14:50:47 +0200

    Fragestellung konfiguriert

--- src/edi/checkapp/content/frage.py
+++ src/edi/checkapp/content/frage.py
@@ -1,60 +1,52 @@
 # -*- coding: utf-8 -*-
-# from plone.app.textfield import RichText
-# from plone.autoform import directives
+from plone.app.textfield import RichText
+from plone.autoform import directives
 from plone.dexterity.content import Item
-# from plone.namedfile import field as namedfile
 from plone.supermodel import model
-# from plone.supermodel.directives import fieldset
-# from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from plone.supermodel.directives import fieldset
+from z3c.form.browser.radio import RadioFieldWidget
+from plone import api as ploneapi
+from z3c.relationfield.schema import RelationChoice, RelationList
+from zope.interface import provider
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary
+from zope import schema
 from zope.interface import implementer
 
 
-# from edi.checkapp import _
+@provider(IContextSourceBinder)
+def possibleQuestionsOrPages(context):
+    brains = ploneapi.content.find(context=context, portal_type=[u'Document', u'Frage'])
+
+    terms = []
+
+    if brains:
+        for i in brains:
+            vocabtitle = "%s (%s)" %(i.Title, i.portal_type)
+            terms.append(SimpleVocabulary.createTerm(i.UID, i.UID, vocabtitle))
+
+    return SimpleVocabulary(terms)
+
 
 
 class IFrage(model.Schema):
     """ Marker interface and Dexterity Python Schema for Frage
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('frage.xml')
+    frage = RichText(title=u"Fragestellung",
+                     description=u"Bitte bearbeiten Sie hier die Frage für die Checkliste")
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    gruppe = schema.TextLine(title=u"Name der Feldgruppe", required=False)
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    optionen = schema.Dict(title=u"Antwortoptionen",
+                           key_type=schema.TextLine(title=u"Antwort"),
+                           value_type=schema.Choice(title=u"Nächste Aktion",
+                                                    description=u"Ohne Auswahl wird die nächste Frage der Liste angezeigt",
+                                                    source=possibleQuestionsOrPages,
+                                                    required=False)
+                          )
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
-
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
-
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
-
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    farbe = schema.Bool(title=u"Hier markieren, wenn die Antwortoptionen im Farbschema dargestellt werden sollen")
 
 
 @implementer(IFrage)
