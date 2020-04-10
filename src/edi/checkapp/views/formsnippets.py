5c8eee36e6636f501ab498e00ddb72a825f775f9
Lars Walther <lwalther@novareto.de>
2020-04-10 23:44:15 +0200

    "Add view: fragestellung-view"

---
+++ src/edi/checkapp/views/formsnippets.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+def option(context):
+    snippet = """\
+<div class="form-check">
+  <input class="form-check-input" type="edi_context_optiontyp" name="edi_context_parentid" id="edi_context_id" value="edi_context_id">
+  <label class="form-check-label" for="edi_context_id">
+    edi_context_title
+  </label>
+</div>"""
+    snippet = snippet.replace("edi_context_id", context.getId())
+    snippet = snippet.replace("edi_context_optiontyp", context.aq_parent.antworttyp)
+    snippet = snippet.replace("edi_context_title", context.title)
+    return snippet
+            
+def option_group(context):
+    snippet = """\
+<div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <div class="input-group-text">
+      <input name="edi_context_parentid" type="edi_context_optiontyp" value="edi_context_id" aria-label="edi_context_title">
+    </div>
+    <span class="input-group-text">edi_context_label</span>
+  </div>
+  <input type="text" class="form-control" aria-label="edi_context_einheit">
+  <div class="input-group-append">
+    <span class="input-group-text">edi_context_einheit</span>
+  </div>
+</div>"""
+    snippet = snippet.replace("edi_context_id", context.getId())
+    snippet = snippet.replace("edi_context_optiontyp", context.aq_parent.antworttyp)
+    snippet = snippet.replace("edi_context_title", context.title)
+    snippet = snippet.replace("edi_context_parentid", context.aq_parent.getId())
+    snippet = snippet.replace("edi_context_label", context.label)
+    snippet = snippet.replace("edi_context_einheit", context.einheit)
+    return snippet
+
+def option_group_label(context):
+    snippet = """\
+<label for="edi_context_id">edi_context_title</label>
+<div id="edi_context_id" class="input-group mb-3">
+  <div class="input-group-prepend">
+    <div class="input-group-text">
+      <input name="edi_context_parentid" type="edi_context_optiontyp" value="edi_context_id"  aria-label="edi_context_title">
+    </div>
+    <span class="input-group-text">edi_context_label</span>
+  </div>
+  <input type="text" class="form-control" aria-label="edi_context_title">
+</div>"""
+    snippet = snippet.replace("edi_context_id", context.getId())
+    snippet = snippet.replace("edi_context_optiontyp", context.aq_parent.antworttyp)
+    snippet = snippet.replace("edi_context_id", context.getId())
+    snippet = snippet.replace("edi_context_title", context.title)
+    snippet = snippet.replace("edi_context_parentid", context.aq_parent.getId())
+    snippet = snippet.replace("edi_context_label", context.label)
+    return snippet
+
+def option_group_label_einheit(context):
+    snippet = """\
+<label for="edi_context_id">edi_context_title</label>
+<div id="edi_context_id" class="input-group mb-3">
+  <div class="input-group-prepend">
+    <div class="input-group-text">
+      <input name="edi_context_parentid" type="edi_context_optiontyp" value="edi_context_id"  aria-label="edi_context_title">
+    </div>
+    <span class="input-group-text">edi_context_label</span>
+  </div>
+  <input type="text" class="form-control" aria-label="edi_context_title">
+  <div class="input-group-append">
+    <span class="input-group-text">edi_context_einheit</span>
+  </div>
+</div>"""
+    snippet = snippet.replace("edi_context_id", context.getId())
+    snippet = snippet.replace("edi_context_optiontyp", context.aq_parent.antworttyp)
+    snippet = snippet.replace("edi_context_id", context.getId())
+    snippet = snippet.replace("edi_context_title", context.title)
+    snippet = snippet.replace("edi_context_parentid", context.aq_parent.getId())
+    snippet = snippet.replace("edi_context_label", context.label)
+    snippet = snippet.replace("edi_context_einheit", context.einheit)
+    return snippet
