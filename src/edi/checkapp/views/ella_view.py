4c81927182638087d9d7c2e967504a6f1f6dca7b
Lars Walther <lwalther@novareto.de>
2021-03-12 09:05:55 +0100

    View für die Ella-App

--- src/edi/checkapp/views/ella_view.py
+++ src/edi/checkapp/views/ella_view.py
@@ -1,11 +1,37 @@
 # -*- coding: utf-8 -*-
-
 from Products.Five.browser import BrowserView
-
+from edi.checkapp.views.examples import uischema
+from plone.i18n.normalizer import idnormalizer
+from plone import api
+import jsonlib
 
 class EllaView(BrowserView):
+
     def __call__(self):
-        template = '''<li class="heading" i18n:translate="">
-          Sample View
-        </li>'''
-        return template
+        content = self.get_content()
+        return jsonlib.write(content)
+
+    def formatcontent(self, obj):
+        article = dict()
+        article['id'] = obj.getId()
+        article['title'] = obj.title
+        article['description'] = obj.description
+        article['text'] = u''
+        if hasattr(obj, 'text'):
+            if obj.text:
+                article['text'] = obj.text.output
+        article['img'] = u''
+        if hasattr(obj, 'image'):
+            if obj.image:
+                article['img'] = '%s/@@download/image' % obj.absolute_url()
+        return article
+
+    def get_content(self):
+        content = []
+        if self.context.portal_type == 'Folder':
+            fc = self.context.listFolderContents(contentFilter={"portal_type" : "Document"})
+            for obj in fc:
+                content.append(self.formatcontent(obj))
+        else:
+            content.append(self.formatcontent(self.context))
+        return content
