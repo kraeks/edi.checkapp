11d129c3e32d7d5287a8e6a3eaa4daf7ce68a6c2
Lars Walther <lwalther@novareto.de>
2021-03-13 12:10:46 +0100

    ella-konfig-view fertiggestellt

--- src/edi/checkapp/views/ella_konfig_view.py
+++ src/edi/checkapp/views/ella_konfig_view.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
-
 from edi.checkapp import _
+from edi.checkapp.content.service import servicetyp
 from Products.Five.browser import BrowserView
 
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-
-
 class EllaKonfigView(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('ella_konfig_view.pt')
 
     def __call__(self):
-        # Implement your own actions:
-        self.msg = _(u'A small message')
+        self.services = self.get_services()
         return self.index()
+
+    def get_services(self):
+        servicelist = list()
+        services = self.context.listFolderContents()
+        for service in services:
+            listentry = dict()
+            listentry['subservices'] = None
+            listentry['title'] = service.title
+            listentry['typ'] = servicetyp.getTerm(service.servicetyp).title
+            if service.servicetyp == 'group':
+                listentry['subservices'] = len(service.listFolderContents(contentFilter={"portal_type" : "Service"}))
+            listentry['url'] = service.absolute_url()
+            servicelist.append(listentry)
+        return servicelist
